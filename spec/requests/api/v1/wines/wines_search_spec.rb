f98e24b23911df2e5762acdc6fd2cce3a6282e81
Alex Schwartz <aschwartz@detaso.com>
2021-04-17 18:15:25 -0500

    Add search result spec for data returned
    Co-authored-by: Wil McCauley <64031242+Wil-McC@users.noreply.github.com>

--- spec/requests/api/v1/wines/wines_search_spec.rb
+++ spec/requests/api/v1/wines/wines_search_spec.rb
@@ -21,25 +21,40 @@ RSpec.describe 'the wine search request' do
       expect(first.keys).to eq([:id, :type, :attributes])
       expect(first[:attributes].keys).to eq([:api_id, :name, :vintage, :region])
 
-      second = data[:data].second
+      second = data[:data].last
       expect(second.keys).to eq([:id, :type, :attributes])
       expect(second[:attributes].keys).to eq([:api_id, :name, :vintage, :region])
     end
 
-    xit "returns correct content" do
+    it "returns correct content" do
       expected_raw = File.read('spec/fixtures/wines_search_results.json')
       expected = JSON.parse!(expected_raw, symbolize_names: true)
 
       stub_microservice_request(expected)
 
-      get api_v1_wines_search_path
+      get api_v1_wines_search_path(region: 'napa', vintage: '2018')
 
       expect(response).to be_successful
       data = JSON.parse(response.body, symbolize_names: true)
-      first = data.first
-      first = data.last
+
+      first = data[:data].first
+      expect(first[:id]).to eq("5f065fb5fbfd6e17acaad294")
+      expect(first[:type]).to eq('wine_search_result')
+      attributes = first[:attributes]
+      expect(attributes[:api_id]).to eq('5f065fb5fbfd6e17acaad294')
+      expect(attributes[:name]).to eq('Duckhorn The Discussion Red 2012')
+      expect(attributes[:vintage]).to eq('2018')
+      expect(attributes[:region]).to eq('Napa Valley')
 
 
+      second = data[:data].last
+      expect(second[:id]).to eq('546e64cf4c6458020000000d')
+      expect(second[:type]).to eq('wine_search_result')
+      attributes = second[:attributes]
+      expect(attributes[:api_id]).to eq('546e64cf4c6458020000000d')
+      expect(attributes[:name]).to eq('Duckhorn')
+      expect(attributes[:vintage]).to eq('2018')
+      expect(attributes[:region]).to eq('Napa Valley')
     end
   end
   # describe 'sad path' do
