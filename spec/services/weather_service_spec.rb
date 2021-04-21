4e71243a1b500800e7023b4a4d63c7ff57521189
Katy La Tour <klatour324@gmail.com>
2021-04-20 23:23:19 -0500

    Fix services specs

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -11,7 +11,37 @@ RSpec.describe WeatherService do
       response = WeatherService.fetch_weather('2015', 'napa valley')
 
       expect(response).to be_an(OpenStruct)
-      # we need to add more expectations to test the structure
+      expect(response).to respond_to("precip")
+      expect(response).to respond_to("temp")
+      expect(response).to respond_to("region")
+      expect(response).to respond_to("start_date")
+      expect(response).to respond_to("end_date")
+    end
+
+    it 'can test the data structure of the API data received from the call' do
+      expected_raw = File.read('spec/fixtures/weather_show_results.json')
+      json_result = JSON.parse!(expected_raw, symbolize_names: true)
+
+      stub_microservice_request(json_result)
+
+      response = WeatherService.fetch_weather('2015', 'napa valley')
+
+      expect(json_result).to be_a(Hash)
+      expect(json_result[:data][:id]).to eq("1")
+      expect(json_result[:data][:type]).to eq("climate_data")
+      expect(json_result[:data][:attributes]).to be_a(Hash)
+      expect(json_result[:data][:attributes]).to have_key(:precip)
+      expect(json_result[:data][:attributes][:precip]).to be_a(String)
+      expect(json_result[:data][:attributes]).to have_key(:temp)
+      expect(json_result[:data][:attributes][:temp]).to be_a(String)
+      expect(json_result[:data][:attributes]).to have_key(:region)
+      expect(json_result[:data][:attributes][:region]).to be_a(String)
+      expect(json_result[:data][:attributes]).to have_key(:vintage)
+      expect(json_result[:data][:attributes][:vintage]).to be_a(String)
+      expect(json_result[:data][:attributes]).to have_key(:start_date)
+      expect(json_result[:data][:attributes][:start_date]).to be_a(String)
+      expect(json_result[:data][:attributes]).to have_key(:end_date)
+      expect(json_result[:data][:attributes][:end_date]).to be_a(String)
     end
   end
   def stub_microservice_request(body)
--- spec/services/wine_service_spec.rb
+++ spec/services/wine_service_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe WineService do
   describe 'happy path' do
     it 'fetches wine data and returns the correct object' do
       expected_raw = File.read('spec/fixtures/wine_show_page_results.json')
-      expected = JSON.parse!(expected_raw, symoblize_names: true)
+      json_result = JSON.parse!(expected_raw, symbolize_names: true)
 
-      stub_microservice_request(expected)
+      stub_microservice_request(json_result)
 
       response = WineService.fetch_wine("5f065fb5fbfd6e17acaad294")
       expect(response).to be_an(OpenStruct)
@@ -15,6 +15,26 @@ RSpec.describe WineService do
       expect(response).to respond_to("name")
       expect(response).to respond_to("vintage")
       expect(response).to respond_to("area")
+    end
+
+    it 'can test the data structure of the API data received from the call' do
+      expected_raw = File.read('spec/fixtures/wine_show_page_results.json')
+      json_result = JSON.parse!(expected_raw, symbolize_names: true)
+
+      stub_microservice_request(json_result)
+
+      response = WineService.fetch_wine("5f065fb5fbfd6e17acaad294")
+
+      expect(json_result).to be_a(Hash)
+      expect(json_result[:data][:attributes]).to be_a(Hash)
+      expect(json_result[:data][:attributes]).to have_key(:api_id)
+      expect(json_result[:data][:attributes][:api_id]).to be_a(String)
+      expect(json_result[:data][:attributes]).to have_key(:name)
+      expect(json_result[:data][:attributes][:name]).to be_a(String)
+      expect(json_result[:data][:attributes]).to have_key(:area)
+      expect(json_result[:data][:attributes][:area]).to be_a(String)
+      expect(json_result[:data][:attributes]).to have_key(:vintage)
+      expect(json_result[:data][:attributes][:vintage]).to be_a(String)
     end
   end
 
