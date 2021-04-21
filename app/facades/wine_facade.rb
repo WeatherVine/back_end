3e2f9ac20849b288cd117a488514adbc3ea4001c
Katy La Tour <klatour324@gmail.com>
2021-04-21 09:35:37 -0500

    Clean up file

--- app/facades/wine_facade.rb
+++ app/facades/wine_facade.rb
@@ -8,18 +8,14 @@ class WineFacade
   end
 
   def self.fetch_all_the_things(wine_api_id)
-    # Need to get weather info
     wine_data = fetch_wine_info(wine_api_id)
 
     weather_data = fetch_weather_info(wine_data.vintage, wine_data.area)
-    # Need to combine the two (and return the value)
+
     combine_data(wine_data, weather_data)
   end
 
   def self.combine_data(wine_data, weather_data)
-    # TODO: should we change how this works
-    #   e.g. leave the wine & weather data to be hashes (& not ostructs)
-    #        So we can save the data transforming time/cost
     merged = wine_data.to_h.merge(weather_data.to_h)
     merged[:id] = nil
     OpenStruct.new(merged)
