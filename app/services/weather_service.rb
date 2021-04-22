951dd437fe533b433305188832c638ea50b4e6a7
Katy La Tour <klatour324@gmail.com>
2021-04-21 22:32:30 -0500

    Hooked up weather microservice to BE. Implemented VCR cassettes for wx service specs
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>
    Co-authored-by: Tommy Nieuwenhuis <72888891+tsnieuwen@users.noreply.github.com>
    Co-authored-by: Trevor Suter <72848529+Trevorsuter@users.noreply.github.com>
    Co-authored-by: Wil McCauley <64031242+Wil-McC@users.noreply.github.com>

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -6,19 +6,19 @@ class WeatherService
   end
 
   def self.fetch_weather(vintage, region)
-    return OpenStruct.new({
-      temp: '50',
-      precip: '0.1',
-      start_date: '2014-01-01',
-      end_date: '2014-12-31'
-      })
+    # return OpenStruct.new({
+    #   temp: '50',
+    #   precip: '0.1',
+    #   start_date: '2014-01-01',
+    #   end_date: '2014-12-31'
+    #   })
     response = fetch_raw_weather_data(vintage, region)
     data = JSON.parse(response.body, symbolize_names: true)
     format_weather_data(data)
   end
 
   def self.fetch_raw_weather_data(vintage, region)
-    weather_connection.get("/climate_data?vintage=#{vintage}&region=#{region}")
+    weather_connection.get("/api/v1/climate_data?vintage=#{vintage}&region=#{region}")
   end
 
   def self.format_weather_data(data)
